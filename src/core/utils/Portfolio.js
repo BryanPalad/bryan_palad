7f174475a82410059def0c1263ccfd6350a88d3d
Bryan Palad <paladbryanj@gmail.com>
2022-11-29 16:19:38 +0800

    add mini project

--- src/core/utils/Portfolio.js
+++ src/core/utils/Portfolio.js
@@ -7,6 +7,7 @@ import collegeProject from "../../assets/img/projects/FernandezDentalClinic.PNG"
 import oldPortfolio from "../../assets/img/projects/oldportfolio.PNG";
 import modernresolve from "../../assets/img/projects/modernresolve.PNG";
 import ezwork from "../../assets/img/projects/ezwork.PNG";
+import sneaks from "../../assets/img/projects/Sneaks.PNG";
 // college project modal
 import slider1 from "../../assets/img/projects/FernandezDentalClinic.PNG";
 import slider2 from "../../assets/img/projects/fdc_signup.PNG";
@@ -18,17 +19,28 @@ import slider5 from "../../assets/img/projects/FDC_dashboard.PNG";
 export const projectsData = [
     {
       id: "1",
+      image: oldPortfolio,
+      name: "My First Portfolio",
+      category: "mini projects",
+      description: 'My first portfolio that helps me refresh my knowledge in HTML, CSS and Javascript, converted into React Stack with emailjs integration.',
+      techStacks: ['React Js','Redux','MUI','Email Js'],
+      pointer: false,
+      demo: "https://bryanpaladfirst.netlify.app/",
+      github: "https://github.com/BryanPalad/my_portfolio",
+    },
+    {
+      id: "2",
       image: mainTesla,
       name: "Tesla UI",
       category: "mini projects",
-      description: 'Mini project that helps me to get familiar in responsive web development (Media queries and flex box).',
+      description: 'Mini project that helps me to get familiar in responsive web development (Media queries, flex box and SCSS).',
       techStacks: ['React Js','Styled Components','SCSS'],
       pointer: false,
       demo: "https://teslaui-eight.vercel.app/",
       github: "https://github.com/BryanPalad/Tesla-clone",
     },
     {
-      id: "2",
+      id: "3",
       image: mainExpense,
       name: "Expense Tracker",
       category: "mini projects",
@@ -39,7 +51,7 @@ export const projectsData = [
       github: "https://github.com/BryanPalad/expenses_tracker_redux",
     },
     {
-      id: "3",
+      id: "4",
       image: mainCrypto,
       name: "Cryptoverse",
       category: "mini projects",
@@ -50,36 +62,26 @@ export const projectsData = [
       github: "https://github.com/BryanPalad/Cryptoverse",
     },
     {
-      id: "4",
+      id: "5",
       image: mainWeather,
       name: "Weather/News App",
       category: "mini projects",
-      description: "A simple app that gets the actual weather status and news depending on the search locations, built in react js, redux toolkit and used 3 API's (Open Weather API's and Bing News Api). ",
+      description: "An app that gets the actual weather status and news, built in react js, redux toolkit and used 3 API's (Open Weather API's and Bing News Api). ",
       techStacks: ['React Js','Redux','3 Api','MUI'],
       pointer: false,
       demo: "https://miniweather-news-app.netlify.app/",
       github: "https://github.com/BryanPalad/Weather-News-App",
     },
     {
-      id: "5",
-      image: oldPortfolio,
-      name: "My First Portfolio",
-      category: "mini projects",
-      description: 'My first portfolio that helps me refresh my knowledge in front end development (HTML, CSS and Javascript), and converted into React Stack with emailjs integration.',
-      techStacks: ['React Js','Redux','MUI','Email Js'],
-      pointer: false,
-      demo: "https://bryanpaladfirst.netlify.app/",
-      github: "https://github.com/BryanPalad/my_portfolio",
-    },
-    {
       id: "6",
-      image: collegeProject,
-      name: "Patient Information and Online Appointment System for Fernandez Dental Clinic",
-      category: "college project",
-      description: 'College capstone that helps dental clinic on their appointment schedules with automatic sms notification etc.',
-      techStacks: ['PHP','CSS','Bootstrap','MySQL'],
-      pointer: true,
-      github: "https://github.com/BryanPalad/FernandezDentalClinic",
+      image: sneaks,
+      name: "Sneaks Ecommerce",
+      category: "mini projects",
+      description: "An Ecommerce project selling different brands  of shoes with cart functionality and responsive web design ",
+      techStacks: ['React Js','Redux','Tailwind'],
+      pointer: false,
+      demo: "https://sneaks-commerce.vercel.app/",
+      github: "https://github.com/BryanPalad/SneaksCommerce",
     },
     {
       id: "7",
@@ -102,6 +104,16 @@ export const projectsData = [
       pointer: false,
       demo: "http://shrouded-bayou-07829.herokuapp.com/",
       github: "https://github.com/BryanPalad/ezWork-app",
+    },
+    {
+      id: "9",
+      image: collegeProject,
+      name: "Patient Information and Online Appointment System for Fernandez Dental Clinic",
+      category: "college project",
+      description: 'College capstone that helps dental clinic on their appointment schedules with automatic sms notification etc.',
+      techStacks: ['PHP','CSS','Bootstrap','MySQL'],
+      pointer: true,
+      github: "https://github.com/BryanPalad/FernandezDentalClinic",
     },
   ];
   
